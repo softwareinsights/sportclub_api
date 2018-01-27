95fe12d289dadf064a8e1b65e7bb7385070616cb
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2018-01-27 14:02:30 -0600

    actividads

--- common/models/place.js
+++ common/models/place.js
@@ -1,5 +1,22 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = function(Place) {
+
+
+    Place.beforeRemote('find', function(ctx, instance, next) {
+        if (!ctx.args.filter) {
+            ctx.args.filter = {};
+        }
+        ctx.args.filter.include = [ "actividads" ];
+        if (!ctx.args.filter.where) {
+            ctx.args.filter.where = {};
+        }
+        ctx.args.filter.where = _.merge(ctx.args.filter.where, { idPerson: { exists : true }});
+        ctx.args.filter.order = ""
+        next();
+    });
+
 
 };
